7b6adc8cc60c97d71cbb8a065df18dc60d311670
Tiago Lima <james.lima.devops@gmail.com>
2022-07-05 17:40:11 -0300

    voltando a versão do codigo

--- app.js
+++ app.js
@@ -2,4 +2,3 @@ console.log("Rodando o sitema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 //mais um console log de teste:
 console.log("Fazendo esse commit somente para testar")
-console.log("Enviando para o GitHub")
